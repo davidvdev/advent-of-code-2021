524c92df6e70e8b629f3ee117e9f75aa8cfd817f
davidvdev <david@davidvdev.com>
2021-12-03 11:15:16 -0600

    pt2 - 1st attempt

--- day-01/puzzle-1-code.js
+++ day-01/puzzle-1-code.js
@@ -3,7 +3,8 @@ const fs = require("fs")
 // create an array that contains all of the input depths
 const pings = fs.readFileSync("./puzzle-1-input.txt").toString().split("\n")
 
-const log = {
+// Part 1
+const logOne = {
     inc: 0,
     dec: 0,
     same: 0
@@ -14,12 +15,34 @@ for (let i = 0; i < pings.length - 1; i++){
     const numI = Number(pings[i])
     const numJ = Number(pings[j])
     if (numI == numJ){
-        log.same++
+        logOne.same++
     } else if (numI > numJ){
-        log.dec++
+        logOne.dec++
     } else if (numI < numJ){
-        log.inc++
+        logOne.inc++
     }
 }
 
-console.log(log)
+// Part 2
+const logTwo = {
+    inc: 0,
+    dec: 0,
+    same: 0
+}
+
+for (let i = 0; i < pings.length - 3; i++){
+    const j = i+1
+    const sumI = Number(pings[i]) + Number(pings[i+1]) + Number(pings[i+2])
+    const sumJ = Number(pings[j]) + Number(pings[j+1]) + Number(pings[j+2])
+
+    if (sumI == sumJ){
+        logTwo.same++
+    } else if (sumI > sumJ){
+        logTwo.dec++
+    } else if (sumI < sumJ){
+        logTwo.inc++
+    }
+}
+
+console.log('logOne: ', logOne)
+console.log('logTwo: ', logTwo)
