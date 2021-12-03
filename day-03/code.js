2b9e78fb532f4ad4a86ea0adbd58c12eb9e62e6c
davidvdev <david@davidvdev.com>
2021-12-03 12:49:50 -0600

    1st attempt brute force

--- day-03/code.js
+++ day-03/code.js
@@ -38,3 +38,46 @@ console.log(gammaArr.join(''))
 console.log(epsilonArr.join(''))
 
 console.log(powerUsed)
+
+// part 2
+// what is the value of gammaArr[0]? keep items from diagnostic that start with that num
+// what is the value of gammaArr[1]? keep items from current list that contain that num in position [1]
+const oxygenRating = () => {
+    const oxygenArr = diagnostic.filter(num => num[0] == gammaArr[0])
+    .filter(num => num[1] == gammaArr[1])
+    .filter(num => num[2] == gammaArr[2])
+    .filter(num => num[3] == gammaArr[3])
+    .filter(num => num[4] == gammaArr[4])
+    .filter(num => num[5] == gammaArr[5])
+    .filter(num => num[6] == gammaArr[6])
+    .filter(num => num[7] == gammaArr[7])
+    .filter(num => num[8] == gammaArr[8])
+    .filter(num => num[9] == gammaArr[9])
+    .filter(num => num[10] == gammaArr[10])
+    // .filter(num => num[11] == gammaArr[11])
+
+    console.log('o2: ', oxygenArr)
+    return oxygenArr
+}
+
+const c02Rating = () => {
+    const c02Arr = diagnostic.filter(num => num[0] == epsilonArr[0])
+    .filter(num => num[1] == epsilonArr[1])
+    .filter(num => num[2] == epsilonArr[2])
+    .filter(num => num[3] == epsilonArr[3])
+    .filter(num => num[4] == epsilonArr[4])
+    .filter(num => num[5] == epsilonArr[5])
+    .filter(num => num[6] == epsilonArr[6])
+    .filter(num => num[7] == epsilonArr[7])
+    .filter(num => num[8] == epsilonArr[8])
+    // .filter(num => num[9] == epsilonArr[9])
+    // .filter(num => num[10] == epsilonArr[10])
+    // .filter(num => num[11] == epsilonArr[11])
+
+    console.log('co2',c02Arr)
+    return c02Arr
+}
+
+const lifeSupport = parseInt(oxygenRating().join(''), 2) * parseInt(c02Rating().join(''), 2)
+console.log(lifeSupport)
+
